d56547cf915b2b275e389be8d94929baad19db24
Trebulkins <trebinivan2005@gmail.com>
2025-01-02 22:11:54 +0700

    browser upd.

--- app/src/main/java/com/example/photogallery2/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery2/PhotoGalleryFragment.kt
@@ -1,5 +1,6 @@
 package com.example.photogallery2
 
+import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
@@ -167,8 +168,21 @@ class PhotoGalleryFragment : VisibleFragment()  {
 
 
 
-    private class PhotoHolder(private val itemImageView: ImageView): RecyclerView.ViewHolder(itemImageView) {
+    private inner class PhotoHolder(private val itemImageView: ImageView): RecyclerView.ViewHolder(itemImageView), View.OnClickListener {
+        private lateinit var galleryItem: GalleryItem
+        init {
+            itemView.setOnClickListener(this)
+        }
+
         val bindDrawable: (Drawable) -> Unit = itemImageView::setImageDrawable
+        fun bindGalleryItem(item: GalleryItem) {
+            galleryItem = item
+        }
+
+        override fun onClick(view: View) {
+            val intent = Intent(Intent.ACTION_VIEW, galleryItem.photoPageUri)
+            startActivity(intent)
+        }
     }
 
     private inner class PhotoAdapter(private val galleryItems: List<GalleryItem>): RecyclerView.Adapter<PhotoHolder>() {
@@ -185,16 +199,16 @@ class PhotoGalleryFragment : VisibleFragment()  {
 
         override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
             val galleryItem = galleryItems[position]
+            holder.bindGalleryItem(galleryItem)
             thumbnailDownloader.queueThumbnail(holder, galleryItem.url)
 
             val placeholder: Drawable = ContextCompat.getDrawable(
                     requireContext(),
-                    R.drawable.ic_launcher_foreground
+                    R.drawable.loading
             )?: ColorDrawable()
             holder.bindDrawable(placeholder)
         }
     }
-
 
     companion object {
         fun newInstance() = PhotoGalleryFragment()
