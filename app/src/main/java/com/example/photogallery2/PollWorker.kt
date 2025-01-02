e5fe79a12885f3572e20afc4ba0d53c84f8309ad
Trebulkins <trebinivan2005@gmail.com>
2025-01-02 21:45:12 +0700

    notifications upd ver.4

--- app/src/main/java/com/example/photogallery2/PollWorker.kt
+++ app/src/main/java/com/example/photogallery2/PollWorker.kt
@@ -3,6 +3,7 @@ package com.example.photogallery2
 import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -55,8 +56,15 @@ class PollWorker(val context: Context, workerParams: WorkerParameters): Worker(c
             val notificationManager = NotificationManagerCompat.from(context)
 
             notificationManager.notify(0, notification)
+            context.sendBroadcast(Intent(ACTION_SHOW_NOTIFICATION), PERM_PRIVATE)
         }
 
         return Result.success()
     }
+
+    companion object {
+        const val PERM_PRIVATE = "com.bignerdranch.android.photogallery.PRIVATE"
+        const val ACTION_SHOW_NOTIFICATION = "com.bignerdranch.android.photogallery.SHOW_NOTIFICATION"
+    }
+
 }
