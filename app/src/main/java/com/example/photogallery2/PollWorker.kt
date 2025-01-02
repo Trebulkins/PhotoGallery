8ee336f79df742bf57080ffaeb635b47f9cb6795
Trebulkins <trebinivan2005@gmail.com>
2025-01-02 21:26:17 +0700

    notifications upd ver.2

--- app/src/main/java/com/example/photogallery2/PollWorker.kt
+++ app/src/main/java/com/example/photogallery2/PollWorker.kt
@@ -1,13 +1,18 @@
 package com.example.photogallery2
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.content.Context
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
 private const val TAG = "PollWorker"
 
 class PollWorker(val context: Context, workerParams: WorkerParameters): Worker(context, workerParams) {
+    @SuppressLint("MissingPermission")
     override fun doWork(): Result {
         val query = QueryPreferences.getStoredQuery(context)
         val lastResultId = QueryPreferences.getLastResultId(context)
@@ -35,6 +40,21 @@ class PollWorker(val context: Context, workerParams: WorkerParameters): Worker(c
         } else {
             Log.i(TAG,"Got a new result: $resultId")
             QueryPreferences.setLastResultId(context, resultId)
+            val intent = PhotoGalleryActivity.newIntent(context)
+            val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+            val resources = context.resources
+            val notification = NotificationCompat
+                    .Builder(context, NOTIFICATION_CHANNEL_ID)
+                    .setTicker(resources.getString(R.string.new_pictures_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentTitle(resources.getString(R.string.new_pictures_title))
+                    .setContentText(resources.getString(R.string.new_pictures_text))
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+                    .build()
+            val notificationManager = NotificationManagerCompat.from(context)
+
+            notificationManager.notify(0, notification)
         }
 
         return Result.success()
